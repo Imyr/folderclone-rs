7258eab7a348e89e616abdc40db41e13aaf25a23
Shashwat Verma <68842852+Imyr@users.noreply.github.com>
2023-03-29 12:50:10 +0530

    Update functions.rs

--- src/functions.rs
+++ src/functions.rs
@@ -1,7 +1,7 @@
 use std::process::exit;
 use google_drive3::api::File;
-use async_recursion::async_recursion;
 use rand::seq::IteratorRandom;
+use async_recursion::async_recursion;
 use google_drive3::{oauth2, DriveHub};
 use google_drive3::hyper::client::HttpConnector;
 use google_drive3::hyper_rustls::HttpsConnector;
